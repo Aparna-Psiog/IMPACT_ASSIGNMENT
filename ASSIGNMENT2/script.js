96c8f0b2075f179d964bee46f3590eb61044a73a
Aparna-Psiog <aparna.j@psiog.com>
2019-08-23 17:49:34 +0530

    Update script.js

--- ASSIGNMENT2/script.js
+++ ASSIGNMENT2/script.js
@@ -1,80 +1,159 @@
 
        var totalpass=0;
        var totalfail=0;
-       var pass=0;
-       var fail=0;
-       var sum,average;
-       var i=1;
-       var j=1;
-       var N=5;
-       var G=2;
+       var pass=["0","0","0","0","0","0","0","0","0","0","0","0","0"];
+       var fail=["0","0","0","0","0","0","0","0","0","0","0","0","0"];
+       var sum=new Array();
+       var average=new Array();
+       var m1=new Array();
+       var m2=new Array();
+       var m3=new Array();
+       var m4=new Array();
+       var N=["0","0","0","0","0","0","0","0","0","0","0","0","0"];
+       var Grades=["0","0","0","0","0","0","0","0","0","0","0","0","0"];
+       var Sections=new Array();
+       var passpercent=["0","0","0","0","0","0","0","0","0","0","0","0","0"];
+       var x= 0;
 
-       while(i<=G)
+    
+
+
+       function myFunction() 
        {
-           while(j<=N)
-           {
-               readmarks();
-               averagecalc();
-               cleartext();
-               j=j+1;
-           }
-   
-
-           i=i+1;
+           x = document.getElementById("ddlFruits").value;
+           document.getElementById("data10").value= x;
        }
-
-
        
 function cleartext(){
     document.getElementById('data1').value=" ";
-
     document.getElementById('mark1').value=" ";
     document.getElementById('mark2').value=" ";
     document.getElementById('mark3').value=" ";
     document.getElementById('mark4').value=" ";
     document.getElementById('mark5').value=" ";
     document.getElementById('mark6').value=" ";
-
 }
 
  function readmarks()
 {
 
-    var m1 = parseInt(document.getElementById('mark1').value,10);
-    var m2 = parseInt(document.getElementById('mark2').value,10);
-    var m3 = parseInt(document.getElementById('mark3').value,10);
-    var m4 = parseInt(document.getElementById('mark4').value,10);
+ 
+    Grades[x] = document.getElementById("ddlFruits").value;
+    Sections[x] = document.getElementById("sections").value;
+
+    m1[x] = parseInt(document.getElementById('mark1').value,10);
+    m2[x] = parseInt(document.getElementById('mark2').value,10);
+    m3[x] = parseInt(document.getElementById('mark3').value,10);
+    m4[x] = parseInt(document.getElementById('mark4').value,10);
+   
+        alert("submitted");
+    averagecalc();
 
 
-        sum= m1+m2+m3+m4;
-        document.getElementById("mark5").value=sum;
-    
+    x++;
+    document.getElementById("text1").value = "";
+
 }
+   
 
 function averagecalc()
 {
-    average=sum/4;
-    document.getElementById("mark6").value=average;
+    sum[x]= m1[x]+m2[x]+m3[x]+m4[x];
+    document.getElementById("mark5").value=sum[x];
+    average[x]=sum[x]/4;
+    document.getElementById("mark6").value=average[x];
 
-    if(average>60)
+    if(average[x]>=60)
     {
-     pass=pass+1;
-     totalpass=pass;
+     pass[Grades[x]]++;
+     N[Grades[x]]++;
+    // totalpass=pass[Grades[x]];
      
     }
-    else{
-        fail=fail+1;
-        totalfail=fail;
+    else { 
+        fail[Grades[x]]++;
+        N[Grades[x]]++;
+        //totalfail=fail[Grades[x]]++;
+       
     }
-            
-    document.getElementById("data4").value=pass;
-
-    document.getElementById("data5").value=fail;
-
-    document.getElementById("data6").value=totalpass;
-
-    document.getElementById("data7").value=totalfail;
+    //passpercent[Grades[x]]=(pass[Grades[x]]/N[Grades[x]])*100;
 }
+
+
+function display_array()
+{
+    var e="<hr/>"; var h ='&nbsp'; var l='&nbsp';
+    for(var s=1;s<=20;s++)
+    {
+        h+="&nbsp";
+    }
+    var k="Grade No.Of Students Pass Students Fail Students Average <br> ";
+    
+    for(var i=1;i<=9;i++)
+    {
+        if(N[i]!=0)
+       { 
+            passpercent=(pass[i]/N[i])*100;
+        e += l+l + i +h+ N[i] +h+ pass[i] +h+ fail[i] +h+ passpercent.toFixed(2)  +"<br/>";
+       }
+       else
+       {
+        e += l+l + i +h+ N[i] +h+ pass[i] +h+ fail[i] +h+ "0" +"<br/>";
+       }
+    }
+    for(var i=10;i<=12;i++)
+    {
+        if(N[i]!=0)
+        {
+            passpercent=(pass[i]/N[i])*100 ;
+        e +=  i +h+ N[i] +h+ pass[i] +h+ fail[i] +h+ passpercent+"<br/>";
+        }
+        else
+        {
+            e +=  i +h+ N[i] +h+ pass[i] +h+ fail[i] +h+ "0" +"<br/>";
+        }
+    }
+
+    document.getElementById("Result").innerHTML=k+e;
+
+}
+
+
+
+function validateForm()
+{
+var x=document.myForm.mark1.value;
+var y=document.myForm.mark2.value;
+var z=document.myForm.mark3.value;
+var u=document.myForm.mark4.value;
+if (x<0 || x >100)
+  {
+  alert("Enter valid mark for english");
+  return false;
+  }
+  if (y<0 || y >100)
+  {
+  alert("Enter valid mark for maths");
+  return false;
+  }
+  if (z<0 || z >100)
+  {
+  alert("Enter valid mark for science");
+  return false;
+  }
+  if (u<0 || u >100)
+  {
+  alert("Enter valid mark for social");
+  return false;
+  }
+
+else {
+   
+    return true;
+}
+
+}
+
 
 
 function Add() {
@@ -91,6 +170,8 @@ function Add1() {
     option.value = document.getElementById("txtValue1").value;
     ddl1.options.add(option);
 }
+
+
 
 
    

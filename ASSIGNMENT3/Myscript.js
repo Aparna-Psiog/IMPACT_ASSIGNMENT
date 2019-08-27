a72028e6ec923320973601b59fda2de0f5e3d88d
Aparna-Psiog <aparna.j@psiog.com>
2019-08-27 18:17:03 +0530

    Update Myscript.js

--- ASSIGNMENT3/Myscript.js
+++ ASSIGNMENT3/Myscript.js
@@ -28,13 +28,53 @@ function div_show() {
 function readvalues(){
     //var records={};
     var records = [
-        { itemName: 'Tomato soup', description: 'Tomato soup-A starter with pieces of breads', price: '100', category: 'Starters' },
-        { itemName: 'Bhel puri', description: 'A chat with tomato sauce', price: '170', category: 'chat' },
-        { itemName: 'French fries', description: 'Fried crisps with Sauce', price: '120', category: 'Starters' },
-        { itemName: 'North Indian Thali', description: 'Meal with chapathi,paratta,Panner butter masala', price: '300', category: 'Maincourse' },
-        { itemName: 'Vanilla Milkshake', description: 'A shake with vanilla flavour with icecream', price: '290', category: 'Desserts' },
-        { itemName: 'South Indian Thali', description: 'Meal with rice,Sambhar,rasam,vegetable,aplam', price: '300', category: 'Maincourse' },
-        { itemName: 'Bhel puri', description: 'A chat with tomato sauce', price: '170', category: 'chat' }
+        { itemName: 'Tomato soup', 
+        description: 'Tomato soup-A starter with pieces of breads', 
+        price: '100',
+        category: 'Starters' },
+
+        { itemName: 'Bhel puri',
+         description: 'A chat with tomato sauce', 
+         price: '170', 
+         category: 'Chat' },
+
+        { itemName: 'French fries',
+         description: 'Fried crisps with Sauce',
+          price: '120', 
+          category: 'Starters' },
+
+        { itemName: 'North Indian Thali', 
+        description: 'Meal with chapathi,paratta,Panner butter masala',
+        price: '300',
+        category: 'Maincourse' },
+
+        { itemName: 'Vanilla Milkshake', 
+        description: 'A shake with vanilla flavour with icecream', 
+        price: '290',
+         category: 'Desserts' },
+
+        { itemName: 'South Indian Thali',
+         description: 'Meal with rice,Sambhar,rasam,vegetable,aplam',
+          price: '300', 
+          category: 'Maincourse' },
+
+        { itemName: 'Pani puri', 
+        description: 'A chat with tomato sauce', 
+        price: '250',
+        category: 'Chat' },
+        { itemName: 'Pani puri', 
+        description: 'A chat with tomato sauce', 
+        price: '250',
+        category: 'Chat' },
+
+        { itemName: 'Pani puri', 
+        description: 'A chat with tomato sauce', 
+        price: '250',
+        category: 'Chat' },
+        { itemName: 'Pani puri', 
+        description: 'A chat with tomato sauce', 
+        price: '250',
+        category: 'Chat' }
 
     ]
     //records["fullName"]=["Aparna"];
@@ -60,18 +100,21 @@ function readFormData() {
 }
 
 function insertNewRecord(data) {
-    var table = document.getElementById("employeeList").getElementsByTagName('tbody')[0];
+    var table = document.getElementById("myTable").getElementsByTagName('tbody')[0];
     var newRow = table.insertRow(table.length);
-    cell1 = newRow.insertCell(0);
+    cell1=newRow.insertCell(0);
+    cell1.innerHTML=`<input id="chk" type="checkbox" value="check"/>`
+    cell1 = newRow.insertCell(1);
     cell1.innerHTML = data.itemName;
-    cell2 = newRow.insertCell(1);
+    cell2 = newRow.insertCell(2);
     cell2.innerHTML = data.description;
-    cell3 = newRow.insertCell(2);
+    cell3 = newRow.insertCell(3);
     cell3.innerHTML = data.price;
-    cell4 = newRow.insertCell(3);
-    cell4.innerHTML = data.category;
     cell4 = newRow.insertCell(4);
-    cell4.innerHTML = `<input type='button' class="Edit" value='Edit' onclick='onEdit(this)'>
+    cell4.innerHTML = data.category;
+    cell4 = newRow.insertCell(5);
+    cell4.innerHTML = 
+    `<input type='button' class="Edit" value='Edit' onclick='onEdit(this)'>
     <input type='button' class="Read" value='Read' onclick='onRead(this)'>
     <input type='button' class="Delete" value='Delete' onclick='onDelete(this)'>`
 }
@@ -86,22 +129,23 @@ function resetForm() {
 
 function onEdit(td) {
     selectedRow = td.parentElement.parentElement;
-    document.getElementById("itemName").value = selectedRow.cells[0].innerHTML;
-    document.getElementById("description").value = selectedRow.cells[1].innerHTML;
-    document.getElementById("price").value = selectedRow.cells[2].innerHTML;
-    document.getElementById("category").value = selectedRow.cells[3].innerHTML;
+    document.getElementById("itemName").value = selectedRow.cells[1].innerHTML;
+    document.getElementById("description").value = selectedRow.cells[2].innerHTML;
+    document.getElementById("price").value = selectedRow.cells[3].innerHTML;
+    document.getElementById("category").value = selectedRow.cells[4].innerHTML;
+    div_show();
 }
 function updateRecord(formData) {
-    selectedRow.cells[0].innerHTML = formData.itemName;
-    selectedRow.cells[1].innerHTML = formData.description;
-    selectedRow.cells[2].innerHTML = formData.price;
-    selectedRow.cells[3].innerHTML = formData.category;
+    selectedRow.cells[1].innerHTML = formData.itemName;
+    selectedRow.cells[2].innerHTML = formData.description;
+    selectedRow.cells[3].innerHTML = formData.price;
+    selectedRow.cells[4].innerHTML = formData.category;
 }
 
 function onDelete(td) {
     if (confirm('Are you sure to delete this record ?')) {
         row = td.parentElement.parentElement;
-        document.getElementById("employeeList").deleteRow(row.rowIndex);
+        document.getElementById("myTable").deleteRow(row.rowIndex);
         resetForm();
     }
 }
@@ -117,3 +161,75 @@ function validate() {
     }
     return isValid;
 }
+
+
+
+    
+
+function deleteselected() 
+{
+var table=document.getElementById("myTable");
+var rowCount=table.rows.length;
+if (confirm('Are you Sure ?')) {
+for (var i=1; i<rowCount; i++) {
+var row=table.rows[i];
+var chkbox=row.cells[0].childNodes[0];
+if (null!=chkbox&&true==chkbox.checked) {
+
+table.deleteRow(i);
+rowCount--;
+i--;
+ }
+}
+}
+}
+
+function getcheckboxes() {
+    var node_list = document.getElementsByTagName('input');
+    var checkboxes = [];
+    for (var i = 0; i < node_list.length; i++) 
+    {
+        var node = node_list[i];
+        if (node.getAttribute('type') == 'checkbox') 
+    {
+            checkboxes.push(node);
+        }
+    } 
+    return checkboxes;
+}
+
+function toggle(source) {
+    checkboxes = getcheckboxes();
+    for(var i=0, n=checkboxes.length;i<n;i++) 
+    {
+      checkboxes[i].checked = source.checked;
+    }
+  }
+  function searchfunction() {
+    // Declare variables 
+    var input, filter, table, tr, td,td1, i, txtValue,txtValue1;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("myTable");
+    tr = table.getElementsByTagName("tr");
+  
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[1];
+      td1= tr[i].getElementsByTagName("td")[4];
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        txtValue1=td1.textContent || td1.innerText;
+        if((txtValue.toUpperCase().indexOf(filter) > -1) ||(txtValue1.toUpperCase().indexOf(filter) > -1))
+         {
+          tr[i].style.display = "";
+        }
+         else 
+         {
+          tr[i].style.display = "none";
+        }
+      } 
+    }
+  }
+
+

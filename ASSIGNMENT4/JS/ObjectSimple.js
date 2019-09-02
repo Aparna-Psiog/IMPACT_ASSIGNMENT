26c3728ba0387c96538595d8868a1bb8bacef8d0
Aparna-Psiog <aparna.j@psiog.com>
2019-09-02 13:39:35 +0530

    Create ObjectSimple.js

---
+++ ASSIGNMENT4/JS/ObjectSimple.js
@@ -0,0 +1,11 @@
+//Creating Object
+
+const foodItem1={
+    Itemname : 'Tomato soup',
+    price    :  100,
+    Category:'Starters',
+    
+    getitemname : function(Itemname){
+      console.log(`This is ${Itemname}`);  
+    }
+};

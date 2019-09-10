e5d20398b3ffccee76b5a803dd7b7aed35dc037d
Aparna-Psiog <aparna.j@psiog.com>
2019-09-10 10:29:21 +0530

    Update Closure.js

--- ASSIGNMENT4/Simple/Closure/Closure.js
+++ ASSIGNMENT4/Simple/Closure/Closure.js
@@ -1,17 +1,26 @@
 const carname='Swift';
 
 function discount_percentage(price,deliverycharge,discount){
+
     var original_price=price;
+
     var deliver=deliverycharge;
+
     var discountvalue=discount;
    
 
-    function calculate(){
+   return function(){
+
         console.log(`The car name is ${carname}`);
-        console.log((original_price+deliver-discountvalue)*100);
-    }
-return calculate();
+        
+        return((original_price+deliver-discountvalue)*100);
+    };
+
 }
 
 
-let dzire= discount_percentage(100000,100,500);
+let dzire= discount_percentage(1000,100,500);
+
+console.log("The original price of the car is"+" "+dzire());
+
+  

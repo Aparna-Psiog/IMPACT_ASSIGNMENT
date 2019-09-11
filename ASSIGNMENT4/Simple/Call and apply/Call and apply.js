5f308359f921b6c31f0b01e027e707eb88c4e251
Aparna-Psiog <aparna.j@psiog.com>
2019-09-11 19:00:11 +0530

    Update Call and apply.js

--- ASSIGNMENT4/Simple/Call and apply/Call and apply.js
+++ ASSIGNMENT4/Simple/Call and apply/Call and apply.js
@@ -10,6 +10,10 @@ var vehicle = [
     name: 'Yamaha'
  }
   ];
+
+
+
+  //using call
   
   for (var i = 0; i < vehicle.length; i++) {
 
@@ -20,13 +24,19 @@ var vehicle = [
       }
       this.print();
     }).call(vehicle[i], i);
+  }
 
-    
+
+
+  //using apply
+  for (var i = 0; i < vehicle.length; i++) {
     (function(i) {
 
-        this.print = function() {
-          console.log('#' + i + ' ' + this.product + ': ' + this.name);
-        }
-        this.print();
-      }).apply[vehicle[i], i];
+      this.print = function() {
+        console.log('#' + i + ' ' + this.product + ': ' + this.name);
+      }
+      this.print();
+    }).apply(vehicle[i], [i]);
   }
+ 
+   

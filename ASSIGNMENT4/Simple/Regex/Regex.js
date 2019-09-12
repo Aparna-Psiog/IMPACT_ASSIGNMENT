356a22f357e53589cbb062bb1273b604ee292442
Aparna-Psiog <aparna.j@psiog.com>
2019-09-12 16:57:58 +0530

    Update Regex.js

--- ASSIGNMENT4/Simple/Regex/Regex.js
+++ ASSIGNMENT4/Simple/Regex/Regex.js
@@ -1,28 +1,33 @@
-var vehicle = function(){
-    var carname="Swift Dzire";
-
-    var reg=new RegExp(/\d+/);
-
-    return {
-        set_name= function(newvalue){
-        if(reg.test(newvalue))
-        {
-            console.log("Invalid car name");
-
-        }
-
-        else{
-            carname=newvalue;
-        }
-    },
-
-    get_name=function()
+function validation(mail)
+{
+    const regex=new RegExp(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
+    if(regex.test(mail))
     {
-        return carname;
+        console.log(mail);
+        console.log("Valid email address");
+    }
+    else{
+        console.log(mail);
+        console.log("Invalid email address");
+    }
+    
+}
+
+function phone_valid(phone_number)
+{
+    const regex1=new RegExp(/^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/)
+    if(regex1.test(phone_number))
+    {
+        console.log(phone_number);
+        console.log("Valid phone number");
+
+    }
+    else{
+        console.log(phone_number);
+        console.log("Invalid phone number");
     }
 }
-}();
-
-console.log(vehicle.get_name());
-vehicle.set_name("Dzire");
-console.log(vehicle.get_name());
+validation("me-info@example.com");
+validation("me-info@example.comgjh");
+phone_valid("032-243-8991");
+phone_valid("04442-243-8991");

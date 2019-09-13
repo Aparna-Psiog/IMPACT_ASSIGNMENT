fd74769f749bdd488ad9fe6bc848bff20b2d9812
Aparna-Psiog <aparna.j@psiog.com>
2019-09-13 16:21:16 +0530

    Create Program.cs

---
+++ ASSIGNMENT5/Assignment_5/Assignment_5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Impact_Assignment5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new int[4];
+            var result = 0;
+            int n = numbers.Length;
+            Console.WriteLine("Enter the array elements:");
+            for (var i = 0; i < n; i++)
+            {
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("The sum of array elements is");
+            for (var i = 0; i < n; i++)
+            {
+
+                result += numbers[i];
+
+            }
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}

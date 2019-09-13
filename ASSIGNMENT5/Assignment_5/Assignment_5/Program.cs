18d9b0cfd64496c483edcb553045550e831c0ab0
Aparna-Psiog <aparna.j@psiog.com>
2019-09-13 18:48:04 +0530

    Update Program.cs

--- ASSIGNMENT5/Assignment_5/Assignment_5/Program.cs
+++ ASSIGNMENT5/Assignment_5/Assignment_5/Program.cs
@@ -13,6 +13,23 @@ namespace Impact_Assignment5
             var numbers = new int[4];
             var result = 0;
             int n = numbers.Length;
+
+            var grades = new List<int>();
+            grades.Add(1);
+            grades.Add(3);
+            grades.Add(8);
+            grades.Add(0);
+            var result1 = 0;
+            
+            
+            foreach(var grade in grades)
+            {
+                result1 += grade;
+            }
+            result1/=grades.Count;
+
+            Console.WriteLine("The average is :"+result1);
+            
             Console.WriteLine("Enter the array elements:");
             for (var i = 0; i < n; i++)
             {

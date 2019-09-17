666215bdc099f12709d01abf29b3d199373dd1aa
Aparna-Psiog <aparna.j@psiog.com>
2019-09-17 19:01:15 +0530

    Create Users.cs

---
+++ ASSIGNMENT5/Hospital_management/Hospital_management/Users.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital_management
+{
+    public class Users
+    {
+        public string username;
+        public string password;
+        private int id;
+
+        public Users(string username, string password, int id)
+        {
+            this.username = username;
+            this.password = password;
+            this.id = id;
+        }
+    }
+}

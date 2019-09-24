0b379f9031c148889ec119b930d23c143f6655c5
Aparna-Psiog <aparna.j@psiog.com>
2019-09-24 19:11:41 +0530

    Update Check_up.cs

--- ASSIGNMENT5/Hospital_management/Hospital_management/Check_up.cs
+++ ASSIGNMENT5/Hospital_management/Hospital_management/Check_up.cs
@@ -7,11 +7,11 @@ namespace Hospital_management
 {
     class Check_up
     {
-        public void Cardiology_list(List<Patient_Details> Patient_name)
+        public void Cardiology_list(List<Patient_Appointment> Patient_name)
         {
             int userOption;
             string whileOption;
-            Patient_Details patientdetails = new Patient_Details();
+            Patient_Appointment patientdetails = new Patient_Appointment();
 
             var Cardiology = new List<Tuple<int, string>>()
             {
@@ -68,11 +68,11 @@ namespace Hospital_management
 
         }
 
-        public void Neurology_list(List<Patient_Details> Patient_name)
+        public void Neurology_list(List<Patient_Appointment> Patient_name)
         {
             int userOption;
             string whileOption;
-            Patient_Details patientdetails = new Patient_Details();
+            Patient_Appointment patientdetails = new Patient_Appointment();
 
             var Neurology= new List<Tuple<int, string>>()
             {
@@ -128,11 +128,11 @@ namespace Hospital_management
 
         }
 
-        public void Dermatology_list(List<Patient_Details> Patient_name)
+        public void Dermatology_list(List<Patient_Appointment> Patient_name)
         {
             int userOption = 0;
             string whileOption;
-            Patient_Details patientdetails = new Patient_Details();
+            Patient_Appointment patientdetails = new Patient_Appointment();
 
 
             var Dermatology = new List<Tuple<int, string>>()
@@ -186,6 +186,67 @@ namespace Hospital_management
                 whileOption = "y";
 
             } while (whileOption == "Y" || whileOption == "y");
+        }
+
+        public void Generalcheckup_list(List<Patient_Appointment> Patient_name)
+        {
+            int userOption;
+            string whileOption;
+            Patient_Appointment patientdetails = new Patient_Appointment();
+
+            var Generalcheckup = new List<Tuple<int, string>>()
+            {
+                new Tuple<int,string>(1,"Check up with Dr.Sheela          "),
+                new Tuple<int,string>(2,"Check up with Dr.Priya       "),
+                new Tuple<int,string>(3,"Blood Test       "),
+                new Tuple<int,string>(4,"Eye Check up      "),
+                new Tuple<int,string>(5,"Tooth check up      "),
+                new Tuple<int,string>(6,"Master check up         "),
+                new Tuple<int,string>(7,"CHeck up with Dr.Akil         "),
+
+            };
+
+            do
+            {
+                Console.WriteLine("\t\t General check up:");
+                Console.WriteLine();
+
+                Console.WriteLine($"{"SNo.",10}\t{"Check Ups",10}");
+                foreach (Tuple<int, string> department in Generalcheckup)
+                {
+                    Console.WriteLine($"{department.Item1,10}\t{department.Item2,10}");
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Enter your option");
+
+                var choiceasString = Console.ReadLine();
+                while (!int.TryParse(choiceasString, out userOption))
+                {
+                    Console.WriteLine("This is not a number!");
+                    choiceasString = Console.ReadLine();
+                }
+
+                while ((userOption > 7) || (userOption <= 0))
+                {
+                    Console.WriteLine("Enter a number less than 7 and greater than 0!");
+                    choiceasString = Console.ReadLine();
+                    while (!int.TryParse(choiceasString, out userOption))
+                    {
+                        Console.WriteLine("This is not a number!");
+                        choiceasString = Console.ReadLine();
+                    }
+                }
+
+
+
+                patientdetails.GetOrder(Generalcheckup, userOption, Patient_name);
+                patientdetails.Display(Patient_name);
+
+                whileOption = "y";
+
+            } while (whileOption == "Y" || whileOption == "y");
+
         }
     }
 }

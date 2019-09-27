56c445a6c5a835959df87a8bf63ce6da707bc787
Aparna-Psiog <aparna.j@psiog.com>
2019-09-27 17:39:26 +0530

    Update Program.cs

--- ASSIGNMENT6/Chatclient/Chatclient/Program.cs
+++ ASSIGNMENT6/Chatclient/Chatclient/Program.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 using System.Net;
 using System.Net.Sockets;
 using System.Threading;
+using System.IO;
+
+
 
 
 
@@ -15,50 +18,146 @@ namespace Chatclient
     {
         static void Main(string[] args)
         {
-            IPAddress ip = IPAddress.Parse("192.168.153.57");
-            int port = 5000;
-            TcpClient client = new TcpClient();
-            client.Connect(ip, port);
-            Console.WriteLine("client connected!!");
-            NetworkStream ns = client.GetStream();
-            Thread thread = new Thread(o => ReceiveData((TcpClient)o));
-
-
-
-            thread.Start(client);
-
-
-
-            string s;
-            while (!string.IsNullOrEmpty((s = Console.ReadLine())))
+            try
             {
-                byte[] buffer = Encoding.ASCII.GetBytes(s);
-                ns.Write(buffer, 0, buffer.Length);
+                IPAddress ip = IPAddress.Parse("192.168.153.57");
+                int port = 5000;
+                TcpClient client = new TcpClient();
+                client.Connect(ip, port);
+                Console.WriteLine("client connected!!");
+                NetworkStream ns = client.GetStream();
+                Thread thread = new Thread(o => ReceiveData((TcpClient)o));
+
+
+
+
+
+
+
+                thread.Start(client);
+
+
+
+
+
+
+
+
+                string fileName;
+                string filePath = @"C:\Users\aparna.j\Desktop\";
+                Console.WriteLine("Enter the name of the file to be sent");
+
+                
+
+
+
+
+                    while (!string.IsNullOrEmpty((fileName = Console.ReadLine())))
+                    {
+                        
+                            byte[] fileNameByte = Encoding.ASCII.GetBytes(fileName);
+
+
+
+
+
+                            byte[] fileData = File.ReadAllBytes(filePath + fileName);
+                            byte[] myData = new byte[4 + fileNameByte.Length + fileData.Length];
+                            byte[] fileNameLen = BitConverter.GetBytes(fileNameByte.Length);
+
+
+
+
+
+                            fileNameLen.CopyTo(myData, 0);
+                            fileNameByte.CopyTo(myData, 4);
+                            fileData.CopyTo(myData, 4 + fileNameByte.Length);
+
+
+
+
+
+
+                            client.Client.Send(myData);
+                            Console.WriteLine("File:{0} has been sent.", fileName);
+                        
+                    }
+
+
+
+
+
+
+
+                
+                client.Client.Shutdown(SocketShutdown.Send);
+                thread.Join();
+                ns.Close();
+                client.Close();
+                Console.WriteLine("disconnect from server!!");
+                Console.ReadKey();
             }
-
-
-
-            client.Client.Shutdown(SocketShutdown.Send);
-            thread.Join();
-            ns.Close();
-            client.Close();
-            Console.WriteLine("disconnect from server!!");
-            Console.ReadKey();
+            catch (Exception ex)
+            {
+                Console.WriteLine("Failed" + ex.Message);
+            }
         }
+
+
+
+
+
+
+
 
 
 
         static void ReceiveData(TcpClient client)
         {
-            NetworkStream ns = client.GetStream();
-            byte[] receivedBytes = new byte[1024];
-            int byte_count;
-
-
-
-            while ((byte_count = ns.Read(receivedBytes, 0, receivedBytes.Length)) > 0)
+            try
             {
-                Console.Write(Encoding.ASCII.GetString(receivedBytes, 0, byte_count));
+                NetworkStream ns = client.GetStream();
+                byte[] receivedBytes = new byte[1024 * 50000];
+                
+
+
+               
+                
+                string receivedPath = @"C:/Users/aparna.j/Desktop/";
+
+
+
+                int receivedBytesLen = client.Client.Receive(receivedBytes);
+
+
+
+                int fileNameLen = BitConverter.ToInt32(receivedBytes, 0);
+                string fileName = Encoding.ASCII.GetString(receivedBytes, 4, fileNameLen);
+
+
+
+                Console.WriteLine("Client:{0} connected & File {1} sharing started.", client.Client.RemoteEndPoint, fileName);
+
+
+
+
+
+
+
+                BinaryWriter bWrite = new BinaryWriter(File.Open(receivedPath + fileName, FileMode.Append));
+                bWrite.Write(receivedBytes, 4 + fileNameLen, receivedBytesLen - 4 - fileNameLen);
+
+
+
+                Console.WriteLine("File: {0} received & saved at path: {1}", fileName, receivedPath);
+
+
+
+                bWrite.Close();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Failed"+ex.Message);
             }
         }
     }

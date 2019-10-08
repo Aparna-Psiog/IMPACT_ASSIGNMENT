8be130627bb299d0eb96edfa6034437159f8d802
Aparna-Psiog <aparna.j@psiog.com>
2019-10-08 19:37:46 +0530

    Update Server.cs

--- ASSIGNMENT6/Textapp/Textserver/Server.cs
+++ ASSIGNMENT6/Textapp/Textserver/Server.cs
@@ -1,15 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-
-
-
-
 
 
 namespace Textserver
@@ -18,7 +13,7 @@ namespace Textserver
     {
        
         static readonly object _lock = new object();
-        static readonly Dictionary<string, TcpClient> list_clients = new Dictionary<string, TcpClient>();
+        static readonly Dictionary<int, TcpClient> list_clients = new Dictionary<int, TcpClient>();
 
 
         static void Main(string[] args)
@@ -27,40 +22,95 @@ namespace Textserver
             int count = 1;
             TcpListener ServerSocket = new TcpListener(IPAddress.Any, 5000);
             ServerSocket.Start();
-            Console.WriteLine("Welcomw to Chat room");
+            Console.WriteLine("Welcome to Chat room");
             while (true)
             {
                 TcpClient client = ServerSocket.AcceptTcpClient();
-                string ipaddress = (client.Client.RemoteEndPoint).ToString();
-                lock (_lock) list_clients.Add(ipaddress, client);
-                Console.WriteLine("Someone connected!!");
+                lock (_lock) list_clients.Add(count, client);
+                Console.WriteLine($"Client {count} connected!!");
+
+
+
                 Thread t = new Thread(handle_clients);
-                t.Start(ipaddress);
+                t.Start(count);
+
+                
+
+
+                Console.WriteLine($"Number of clients connected {count}");
                 count++;
             }
         }
+       
         public static void handle_clients(object o)
         {
-            string id = (string)o;
+            int id = (int)o;
             TcpClient client;
             lock (_lock) client = list_clients[id];
+
             while (true)
             {
                 NetworkStream stream = client.GetStream();
                 byte[] buffer = new byte[1024];
                 int byte_count = stream.Read(buffer, 0, buffer.Length);
-                if (byte_count == 0)
+                string messagedata = Encoding.ASCII.GetString(buffer, 0, byte_count);
+                if(messagedata == "message")
                 {
-                    break;
+                    byte[] buffernew = new byte[1024];
+                    int byte_count_new = stream.Read(buffernew, 0, buffernew.Length);
+                    if (byte_count_new == 0)
+                    {
+                        break;
+                    }
+
+
+
+                    string data = Encoding.ASCII.GetString(buffer, 0, byte_count);
+                    broadcast(data);
+                    Console.WriteLine(data);
                 }
-                string data = Encoding.ASCII.GetString(buffer, 0, byte_count);
-                broadcast(data);
-                Console.WriteLine(data);
+                else
+                {
+                    try
+                    {
+
+                        
+                        byte[] clientData = new byte[1024 * 5000];
+
+
+
+                        int receivedBytesLen = client.Client.Receive(clientData);
+                        Console.WriteLine(receivedBytesLen);
+                        if (receivedBytesLen == 0)
+                        {
+                            break;
+                        }
+                        foreach (TcpClient c in list_clients.Values)
+                        {
+                            //stream.Write(clientData, 0, clientData.Length);
+                            c.Client.Send(clientData);
+                            
+                        }
+
+
+
+
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("File Sending fail." + ex.Message);
+                    }
+                }
             }
+
+
             lock (_lock) list_clients.Remove(id);
             client.Client.Shutdown(SocketShutdown.Both);
             client.Close();
         }
+
+        
+    
         public static void broadcast(string data)
         {
             byte[] buffer = Encoding.ASCII.GetBytes(data + Environment.NewLine);

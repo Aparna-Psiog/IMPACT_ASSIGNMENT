b06c20e3d78ec606ad0a3a1dad61a65ba631fcc7
Aparna-Psiog <aparna.j@psiog.com>
2019-10-30 15:29:39 +0530

    Create patientAppointment.cs

---
+++ ASSIGNMENT5/Hospital_management/Hospital_management/patientAppointment.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Hospital_management
+{
+    public class Patient_Appointment
+    {
+        public int SNo = 1;
+        public string Checkup_Name;
+        public string Date_Timing;
+        public string Correct_time;
+
+        //public object Patient_name { get; private set; }
+
+        static bool DateCheck(string stringdate)
+        {
+            // Define the acceptable date formats and check if entered date is valid
+            string[] formats = { "d/MM/yyyy", "dd/MM/yyyy", "d/M/yyyy" };
+            string bdate = stringdate;
+
+            DateTime parsedDate;
+            try
+            {
+                //Conversion from string to Date time
+                bool isValidFormat = DateTime.TryParseExact(stringdate, formats, new CultureInfo("ta-IN"), DateTimeStyles.None, out parsedDate);
+
+                if (isValidFormat)
+                {
+                    // If date is valid, check whether the given year is more than the year Today
+                    if (parsedDate<DateTime.Now)
+
+                    {
+                        Console.WriteLine("Appointment date must be appropriate");
+                        return false;
+                    } 
+                    
+                    else
+                    {
+                        //end do while Loop
+                        return true;
+                    }
+                }
+                else
+                {
+                    //if not a valid date, give an error
+                    Console.WriteLine("Wrong Date format");
+                    return false;
+                }
+
+            }
+            catch (FormatException)
+            {
+                //Catch format exception errors on date
+                return false;
+            }
+        }
+        static int CalculateAge(string dateOfbirth)
+        {
+            DateTime birthDay = DateTime.Parse(dateOfbirth);
+            int years = DateTime.Now.Year-birthDay.Year;
+            return years;
+        }
+        static int Calculatemonth(string dateOfbirth)
+        {
+            DateTime birthDay = DateTime.Parse(dateOfbirth);
+            int month = DateTime.Now.Month-birthDay.Month;
+            return month;
+        }
+
+        static double Calculatedate(string dateOfbirth)
+        {
+            DateTime birthDay = DateTime.Parse(dateOfbirth);
+            double date = (DateTime.Now.Date-birthDay.Date).TotalDays;
+            return date;
+        }
+
+        public void GetOrder(List<Tuple<int, string>> department, int userOption, List<Patient_Appointment> Patient_name)
+        {
+            Patient_Appointment patientdetails = new Patient_Appointment();
+            foreach (Tuple<int, string> tuple in department)             //loop inside the Tuple
+            {
+                if (tuple.Item1 == userOption)
+                {
+                    patientdetails.Checkup_Name = tuple.Item2;
+                   // order.Price = tuple.Item3;
+                }
+            }
+
+            Console.WriteLine("Enter the date of appointment:");
+            do
+            {
+                //Prompt and obtain user input 
+                //get Date of Birth
+                Console.Write("Date of Appointment: (dd/mm/yyyy): ");
+                patientdetails.Date_Timing = Console.ReadLine();
+
+                //validate date input if empty and if satisfies formatting conditions
+                if (!string.IsNullOrEmpty (patientdetails.Date_Timing) && DateCheck( patientdetails.Date_Timing ))
+                {
+                    //end loop
+                    break;
+                }
+
+                //Ask the user to repeatedly enter the value until a valid value has been entered
+            } while (true);
+
+            patientdetails.Correct_time = patientdetails.Date_Timing;
+            Patient_name.Add(patientdetails);
+        }
+
+        public void Display(List<Patient_Appointment> Patient_name)
+        {
+            //long TotalCost = 0;
+            int Option = 0;
+            string Getoption;
+            bool Confirmresult = true;
+
+            Console.Clear();
+           
+            Console.WriteLine("-----------------------FrontLine Hospital appointment -----------------------");
+            Console.WriteLine("------------------------------------------------------------------------------");//Displaying the order
+            Console.WriteLine($"{"SNo",13}" + "\t" + $"{"Checkup_Name",13}" + "\t" +"\t"+$"{"Appointment Date",13}");
+            Console.WriteLine("------------------------------------------------------------------------------");
+            Console.WriteLine();
+
+            SNo = 1;
+            foreach (Patient_Appointment i in Patient_name)
+            {
+                i.SNo = SNo;
+                Console.WriteLine($"{i.SNo,10} \t  {i.Checkup_Name,10}{i.Correct_time,10}");
+                SNo += 1;
+            }
+            Console.WriteLine("------------------------------------------------------------------------------");
+
+
+            Console.WriteLine("Which Operation you want to perform ");
+            Console.WriteLine("1. Confirm");
+            Console.WriteLine("2. Update");
+            Console.WriteLine("3. Delete");
+            Console.WriteLine("4. Continue!");
+            Console.WriteLine("Enter your Option");
+
+            do
+            {
+                Getoption = Console.ReadLine();
+                while (!int.TryParse(Getoption, out Option))
+                {
+                    Console.WriteLine("This is not a number!");
+                    Getoption = Console.ReadLine();
+                }
+                if ((Option != 1) && (Option != 2) && (Option != 3) && (Option != 4))
+                {
+                    Console.WriteLine("Enter PROPER operation");
+                    Confirmresult = true;
+                }
+                else Confirmresult = false;
+
+            } while (Confirmresult == true);
+
+            switch (Option)      //Switch case for chosing what to do
+            {
+                case 1:
+                    Appointment.Confirm(Patient_name);
+                    break;
+
+                case 2:
+                    Appointment.Update(Patient_name);
+                    break;
+
+                case 3:
+                    Appointment.Delete(Patient_name);
+                    break;
+
+            }
+        }
+
+        internal static void RemoveAll(Func<object, bool> p)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

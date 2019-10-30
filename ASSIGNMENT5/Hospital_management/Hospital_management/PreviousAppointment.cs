c37c39f4a55d4b7486b35b5be2cd874c984998aa
Aparna-Psiog <aparna.j@psiog.com>
2019-10-30 15:29:47 +0530

    Update PreviousAppointment.cs

--- ASSIGNMENT5/Hospital_management/Hospital_management/PreviousAppointment.cs
+++ ASSIGNMENT5/Hospital_management/Hospital_management/PreviousAppointment.cs
@@ -9,34 +9,35 @@ namespace Hospital_management
     {
         public static void AddAppoint(List<Patient_Appointment> Patient_name)
         {
-            int TotalCost = 0;
+            
             var SNo = 1;
             DateTime now = DateTime.Now;
-            StreamWriter sw = File.AppendText(@"Appointments.txt");
+            StreamWriter sw1 = File.AppendText(@"Appointments.txt");
 
 
 
 
-            sw.WriteLine("-----------------------FrontLine Hospital appointment ------------------------");
+            sw1.WriteLine("-----------------------FrontLine Hospital appointment ------------------------");
 
-            sw.WriteLine("------------------------------------------------------------------------------");//Displaying the order
-            sw.WriteLine($"{"SNo",13}" + "\t" + $"{"Checkup_Name",13}" + "  " + "  " + "  " + "  " + "  " + $"{"Appointment Date",13}");
+            sw1.WriteLine("------------------------------------------------------------------------------");//Displaying the order
+            sw1.WriteLine($"{"SNo",13}" + "\t" + $"{"Checkup_Name",13}" + "  " + "  " + "  " + "  " + "  " + $"{"Appointment Date",13}");
 
-            sw.WriteLine("------------------------------------------------------------------------------");
-            sw.WriteLine();
+            sw1.WriteLine("------------------------------------------------------------------------------");
+            sw1.WriteLine();
 
             SNo = 1;
             foreach (Patient_Appointment i in Patient_name)
             {
                 i.SNo = SNo;
-                // i.Total = i.Price * i.Quantity;
-                sw.WriteLine($"{i.SNo,10} \t  {i.Checkup_Name,10}{i.Correct_time,10}");
+       
+                sw1.WriteLine($"{i.SNo,10} \t  {i.Checkup_Name,10}{i.Correct_time,10}");
 
                 SNo += 1;
             }
-            sw.WriteLine("-------------------------------------------------------------------------------");
+            sw1.WriteLine("-------------------------------------------------------------------------------");
          
-            sw.Close();
+            sw1.Close();
+           
         }
     }
 }
